6071baaad341f9daa111ecc9e0547b345b18dcb9
h21 <49756324+DeLanau@users.noreply.github.com>
2019-07-09 17:01:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,7 +41,7 @@ bot.on("message", async message => {
 
     let prefix = botconfig.prefix;
     let msgArray = message.content.split(" ");
-    let args = messageArray.slice(1);
+    let args = msgArray.slice(1);
     let cmd = msgArray[0];
     
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
