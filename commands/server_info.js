e439e71e381bcc476874e551bf46fa8a3c0ebdbb
h21 <49756324+DeLanau@users.noreply.github.com>
2019-07-09 17:43:02 +0200

    Update server_info.js

--- commands/server_info.js
+++ commands/server_info.js
@@ -2,17 +2,9 @@ const Discord= require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    let server_avatar = message.guild.displayAvatarURL;
-    
-    message.channel.send(new Discord.RichEmbed()
-    .setTitle("Title")
-    .setAuthor("Author")
-    .setColor("#ff0066")
-    .setDescription("Server info")
-    .setTumbnail(server_avatar)
-    .addField("Server name", message.guild.name)
-    .addField("Total users", message.guild.memerCount)).catch((e) => { console.log(e); });
-
+    message.channel.send("Информация о сервере:\n" + "Название сервера: " + message.guild.name + "/n"
+    + "Количество пользователей: " + message.guild.memberCount)    
+   
 }
 
 module.exports.help = {
