b740c7039bfd2a700c340a22d9ef2a469e65343c
h21 <49756324+DeLanau@users.noreply.github.com>
2019-07-09 16:23:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ bot.on('ready', async () => {
 
     console.log(`${bot.user.username} is ready!`);
 
-    bot.user.setActivity("Anime", {type: "WATCHING"})
+    bot.user.setActivity('Anime', {type: 'WATCHING'})
     
 });
 
