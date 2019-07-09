59c22a15450b89e075d3cb002815e49fe7a8e2fa
h21 <49756324+DeLanau@users.noreply.github.com>
2019-07-09 18:27:36 +0200

    1

--- commands/bot_info.js
+++ commands/bot_info.js
@@ -3,7 +3,7 @@ const Discord= require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     message.channel.send("Информация о БОТЕ:\n" + "Название Бота: " + bot.user.username +
-    + "\nСоздатель и Разработчик: " + "Happik21 - можете закидать формами" + "\nФункционал: " + "В разработке")    
+    "\nСоздатель и Разработчик: " + "Happik21 - можете закидать формами" + "\nФункционал: " + "В разработке")    
    
 }
 
--- commands/server_info.js
+++ commands/server_info.js
@@ -2,7 +2,7 @@ const Discord= require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    message.channel.send("Информация о сервере:\n" + "Название сервера: " + message.guild.name +
+    message.channel.send("Информация о сервере:\n" + "Название сервера: " + message.guild.name
     + "\nКоличество пользователей: " + message.guild.memberCount)    
    
 }
