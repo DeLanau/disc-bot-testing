341eb29e351cb1b64b34c60550af2fbb2aa725c1
h21 <49756324+DeLanau@users.noreply.github.com>
2019-07-09 17:18:18 +0200

    Update server_info.js

--- commands/server_info.js
+++ commands/server_info.js
@@ -5,13 +5,15 @@ module.exports.run = async (bot, message, args) => {
     let server_avatar = message.guild.displayAvatarURL;
 
     const embed = new RichEmbed()
-    .setDescription("Server info")
+    .setTitle("Title")
+    .setAuthor("Author")
     .setColor("#ff0066")
+    .setDescription("Server info")
     .setTumbnail(server_avatar)
     .addField("Server name", message.guild.name)
     .addField("Total users", message.guild.memerCount);
 
-    message.channel.send(embed);
+    message.channel.send({embed});
 }
 
 module.exports.help = {
