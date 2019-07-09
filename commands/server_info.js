13bce83a0f6f87c4c739b9ee1076e5aa8eaf0a00
h21 <49756324+DeLanau@users.noreply.github.com>
2019-07-09 17:15:04 +0200

    Update server_info.js

--- commands/server_info.js
+++ commands/server_info.js
@@ -1,5 +1,4 @@
-const Discord = require("discord.js");
-const RichEmbed = require("discord.js");
+const { Discord, RichEmbed }= require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
