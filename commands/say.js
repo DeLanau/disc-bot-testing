78ddb192dbce2b1d2e49f15a8116801b36884a02
h21 <49756324+DeLanau@users.noreply.github.com>
2019-07-09 18:23:17 +0200

    1

---
+++ commands/say.js
@@ -0,0 +1,24 @@
+const Discord= require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    if(!message.member.id === "266556791440146432") return message.channel.send("Ты не достоен")
+
+    let argsresult
+    let mChannel = message.mentions.channel.first();
+
+    message.delet()
+    if(mChannel){
+        argsresult = args.slice(1).join(" ")
+        mChannel.send(argsresult)
+    }else {
+        argsresult = args.join(" ")
+        message.channel.send(argsresult)
+    }
+   
+}
+
+module.exports.help = {
+    name: "say",
+    aliases: ["s", "msg"]
+}
