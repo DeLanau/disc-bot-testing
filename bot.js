628181d93078974a01d6b51b5efd723aaf97c332
h21 <49756324+DeLanau@users.noreply.github.com>
2019-09-09 10:15:06 +0200

    1

--- bot.js
+++ bot.js
@@ -82,43 +82,7 @@ bot.on('voiceStateUpdate', (oldMember, newMember) => {
             bot.channels.get(`540590997143552013`).send('<@&620323100084535316>').then(msg => {
                 msg.delete(10000)});
   
-        }else if(newMember.user.id == '266556791440146432'){
-
-            newMember.voiceChannel.join().then(connection => { 
-                console.log("Joined voice channel!");
-
-                let file = path.resolve("./voice.mp3")
-                console.log("File: " + file);
-        
-                const dispatcher = connection.playFile(path);
-        
-                let start = 0;
-                let end = 0;
-        
-                dispatcher.on('start', () => {
-                    start = new Date();
-                    start = start.getTime();
-                    connection.player.streamingData.pausedTime = 0;
-                    dispatcher.setVolume(0.70);
-                    console.log("Playing");
-        
-                });
-        
-                dispatcher.on('debug', (info) => {
-                    console.log("Debug: " + info);
-                });
-        
-                dispatcher.on('end', reason => {
-                    console.log("Finished");
-                    end = new Date();
-                    end = end.getTime();
-                    console.log("Playtime: " + (end - start) + "ms");
-                    console.log("End: " + reason);
-                });
-            });
-            
         }
-    
     } else if(newUserChannel === undefined){
   
       // User leaves a voice channel
